26d156837793e82069984ce879bde7600096b8d7
AMT <amtamt547@gmail.com>
2023-10-10 11:01:35 +0630

    add migration

---
+++ database/migrations/2023_10_10_042315_create_request_music_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('request_music', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_name');
+            $table->string('music_name');
+            $table->string('anime_name')->nullable();
+            $table->longText('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('request_music');
+    }
+};
